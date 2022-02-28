7a7eafc85e03acd7a85ce6c61e3c798fa05d2d30
Maksym Zhakun <mzhakun@qunatifycrypto.net>
2022-02-28 00:48:54 -0600

    DEFI-46 Curve Pools

---
+++ types/apollo/main/graphql.d.ts
@@ -0,0 +1,35 @@
+
+declare module '*/config.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const GlobalStatsQueryGQL: DocumentNode;
+export const RecentPricesGQL: DocumentNode;
+export const ChainlinkEthUsdPriceGQL: DocumentNode;
+export const EthAbiGQL: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/pools.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const AllProtocolsGQL: DocumentNode;
+export const CurvePoolsGQL: DocumentNode;
+export const AavePoolGQL: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/portfolio.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const BalancesGQL: DocumentNode;
+export const ProtocolGQL: DocumentNode;
+export const TransactionsGQL: DocumentNode;
+export const TransactionLogEventsGQL: DocumentNode;
+
+  export default defaultDocument;
+}
+    
