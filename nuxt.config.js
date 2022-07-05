da3a6294e8015fa6e1f205c3c9ef003818c7465a
Irene Aguas <iaguas@quantifycrypto.net>
2022-07-05 13:57:05 -0700

    add mobile viewport on nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,15 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: { link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }] },
+  head: {
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+  },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
