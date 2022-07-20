e120f255edc5a8aa694d7e45dd3dd271fcdbe573
Maksym Zhakun <mzhakun@qunatifycrypto.net>
2022-07-20 00:21:30 -0500

    DEFI-90 Uniswap Vue Component (initial deployment)

---
+++ types/apollo/main/graphql.d.ts
@@ -0,0 +1,42 @@
+
+declare module '*/config.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const SupportedChainsGQL: DocumentNode;
+export const GasGQL: DocumentNode;
+export const DeFiStats: DocumentNode;
+export const RecentPricesGQL: DocumentNode;
+export const UniswapTokensGQL: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/pools.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const AavePoolGQL: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/portfolio.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const BalancesGQL: DocumentNode;
+export const TransactionsGQL: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/token.query.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const TokenQueryGQL: DocumentNode;
+export const DailyChartGQL: DocumentNode;
+
+  export default defaultDocument;
+}
+    
