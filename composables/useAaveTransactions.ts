58a3e3c54dd7b6deb933f8fa303f9ce4eccc81ec
Maksym Zhakun <mzhakun@qunatifycrypto.net>
2022-08-06 06:11:39 -0500

    DEFI-90 Uniswap Vue Component (initial deployment)

--- composables/useAaveTransactions.ts
+++ composables/useAaveTransactions.ts
@@ -3,25 +3,34 @@ import { ethers } from 'ethers'
 import type { AavePoolModel } from '~/composables/useAavePools'
 import { Web3, WEB3_PLUGIN_KEY } from '~/plugins/web3/web3'
 import wethGatewayAbi from '~/constracts/abi/aave/wethGatewayAbi.json'
-import erc20Abi from '~/constracts/abi/erc20Abi.json'
 import lendingPoolAbi from '~/constracts/abi/aave/lendingPoolAbi.json'
+import { AaveLendingPoolContract } from '~/types/abi/aave-lending-pool-contract'
+import { AaveWethPoolContract } from '~/types/abi/aave-weth-pool-contract'
+import useERC20 from '~/composables/useERC20'
+import { useHelpers } from '~/composables/useHelpers'
 
 interface MarketConfig {
   name: string
   AAVE_LENDING_POOL_ADDRESS: string
   AAVE_WETH_GATEWAY_ADDRESS: string
+  AWETH_ADDRESS: string
+  WETH_VARIABLE_BORROW: string
 }
 const configs: { [key: number]: MarketConfig } = {
   1: {
     name: 'ETH mainnet',
     AAVE_LENDING_POOL_ADDRESS: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
     AAVE_WETH_GATEWAY_ADDRESS: '0xcc9a0B7c43DC2a5F023Bb9b738E45B0Ef6B06E04',
+    AWETH_ADDRESS: '0x030bA81f1c18d280636F32af80b9AAd02Cf0854e',
+    WETH_VARIABLE_BORROW: '0xF63B34710400CAd3e044cFfDcAb00a0f32E33eCf',
   },
 
   137: {
     name: 'Polygon (Matic) ',
     AAVE_LENDING_POOL_ADDRESS: '0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf',
     AAVE_WETH_GATEWAY_ADDRESS: '0xbEadf48d62aCC944a06EEaE0A9054A90E5A7dc97',
+    AWETH_ADDRESS: '',
+    WETH_VARIABLE_BORROW: '',
   },
 
   // 1337: {
@@ -34,12 +43,10 @@ const configs: { [key: number]: MarketConfig } = {
     name: 'ETH mainnet',
     AAVE_LENDING_POOL_ADDRESS: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
     AAVE_WETH_GATEWAY_ADDRESS: '0xcc9a0B7c43DC2a5F023Bb9b738E45B0Ef6B06E04',
+    AWETH_ADDRESS: '0x030bA81f1c18d280636F32af80b9AAd02Cf0854e',
+    WETH_VARIABLE_BORROW: '0xF63B34710400CAd3e044cFfDcAb00a0f32E33eCf',
   },
 }
-
-const MAX_UINT256 = '115792089237316195423570985008687907853269984665640564039457584007913129639935'
-const ETH_GAS_LIMIT_WEI = 400000
-const ETH_GAS_PRICE_GWEI = 16
 
 interface Transaction {
   loading: boolean
@@ -49,7 +56,9 @@ interface Transaction {
 
 export default function useAaveTransactions(pool: Ref<AavePoolModel>, amountInTokens: Ref<number>) {
   // COMPUTED
-  const { signer, provider, account, chainId } = inject(WEB3_PLUGIN_KEY) as Web3
+  const { signer, account, chainId } = inject(WEB3_PLUGIN_KEY) as Web3
+  const { allowedSpending, approveMaxSpending } = useERC20()
+  const { isNativeToken } = useHelpers()
 
   // STATE
   const txData = reactive<Transaction>({
@@ -67,6 +76,13 @@ export default function useAaveTransactions(pool: Ref<AavePoolModel>, amountInTo
     chainId.value && chainId.value in configs ? configs[chainId.value].AAVE_WETH_GATEWAY_ADDRESS : ''
   )
 
+  const AWETH_ADDRESS = computed(() =>
+    chainId.value && chainId.value in configs ? configs[chainId.value].AWETH_ADDRESS : ''
+  )
+  // const WETH_VARIABLE_BORROW = computed(() =>
+  //   chainId.value && chainId.value in configs ? configs[chainId.value].WETH_VARIABLE_BORROW : ''
+  // )
+
   const txLoading = computed(() => txData.loading)
   const receipt = computed(() => txData.receipt)
   const isTxMined = computed(() => !!(txData.receipt && txData.receipt.transactionHash && txData.receipt.blockNumber))
@@ -78,140 +94,113 @@ export default function useAaveTransactions(pool: Ref<AavePoolModel>, amountInTo
     txData.receipt = null
   }
 
-  const allowedSpending = async (): Promise<number> => {
-    try {
-      const tokenInstance = new ethers.Contract(pool.value.underlyingAsset, erc20Abi, signer.value)
-      const checkApprove = await tokenInstance.populateTransaction.allowance(account.value, AAVE_LENDING_POOL_ADDRESS)
-      const approved = await provider.value.call(checkApprove)
-      return parseInt(approved)
-    } catch (err) {
-      return 0
-    }
-  }
-
-  const isSpendingApproved = async () =>
-    ['ETH', 'MATIC'].includes(pool.value.symbol) ? true : (await allowedSpending()) > amountInTokens.value
-
-  async function approveMaxSpendingWeb3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
-    try {
-      const erc20 = new ethers.Contract(pool.value.underlyingAsset, erc20Abi, signer.value)
-      const approveERC20 = await erc20.populateTransaction.approve(AAVE_LENDING_POOL_ADDRESS.value, MAX_UINT256)
-      const txGasLimit = await provider.value.estimateGas(approveERC20)
-      const txGasPrice = await provider.value.getGasPrice()
-      approveERC20.gasLimit = txGasLimit
-      approveERC20.gasPrice = txGasPrice
-      const approveResult = await signer.value.sendTransaction(approveERC20)
-      const resp = await approveResult.wait()
-      return { isCompleted: true, receipt: resp }
-    } catch (error) {
-      return { isCompleted: false, receipt: error }
-    }
-  }
-
   async function depositETHWeb3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
     try {
-      const wethGatewayContract = new ethers.Contract(AAVE_WETH_GATEWAY_ADDRESS.value, wethGatewayAbi, signer.value)
+      const contract = new ethers.Contract(
+        AAVE_WETH_GATEWAY_ADDRESS.value,
+        wethGatewayAbi,
+        signer.value
+      ) as unknown as AaveWethPoolContract
       const amount = ethers.utils.parseUnits(`${amountInTokens.value}`, pool.value.decimals)
-      const depositCall = await wethGatewayContract.populateTransaction.depositETH(
+      const depositCall = await contract.depositETH(AAVE_LENDING_POOL_ADDRESS.value, account.value, 0, {
+        value: amount,
+      })
+      const resp = await depositCall.wait()
+      return { isCompleted: true, receipt: resp }
+    } catch (error) {
+      return { isCompleted: false, receipt: error }
+    }
+  }
+  async function withdrawETHWeb3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
+    try {
+      const contract = new ethers.Contract(
+        AAVE_WETH_GATEWAY_ADDRESS.value,
+        wethGatewayAbi,
+        signer.value
+      ) as unknown as AaveWethPoolContract
+      const amount = ethers.utils.parseUnits(`${amountInTokens.value}`, pool.value.decimals)
+      const call = await contract.withdrawETH(AAVE_LENDING_POOL_ADDRESS.value, amount, account.value)
+      const resp = await call.wait()
+      return { isCompleted: true, receipt: resp }
+    } catch (error) {
+      return { isCompleted: false, receipt: error }
+    }
+  }
+
+  async function repayETHWeb3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
+    try {
+      const contract = new ethers.Contract(
+        AAVE_WETH_GATEWAY_ADDRESS.value,
+        wethGatewayAbi,
+        signer.value
+      ) as unknown as AaveWethPoolContract
+      const amount = ethers.utils.parseUnits(`${amountInTokens.value}`, pool.value.decimals)
+      const call = await contract.repayETH(AAVE_LENDING_POOL_ADDRESS.value, amount, 2, account.value)
+      const resp = await call.wait()
+      return { isCompleted: true, receipt: resp }
+    } catch (error) {
+      return { isCompleted: false, receipt: error }
+    }
+  }
+
+  async function depositERC20Web3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
+    try {
+      const contract = new ethers.Contract(
         AAVE_LENDING_POOL_ADDRESS.value,
-        account.value,
-        0
-      )
-
-      depositCall.gasLimit = ethers.utils.parseUnits(`${ETH_GAS_LIMIT_WEI}`, 'wei')
-      depositCall.gasPrice = ethers.utils.parseUnits(`${ETH_GAS_PRICE_GWEI}`, 'gwei')
-      depositCall.value = amount
-
-      const depositResult = await signer.value.sendTransaction(depositCall)
-      const resp = await depositResult.wait()
-
+        lendingPoolAbi,
+        signer.value
+      ) as unknown as AaveLendingPoolContract
+      const amount = ethers.utils.parseUnits(`${amountInTokens.value}`, pool.value.decimals)
+      const depositCall = await contract.deposit(pool.value.underlyingAsset, amount, account.value, 0)
+      const resp = await depositCall.wait()
       return { isCompleted: true, receipt: resp }
     } catch (error) {
       return { isCompleted: false, receipt: error }
     }
   }
 
-  async function depositWeb3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
+  async function borrowERC20Web3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
     try {
-      const lendingPoolContract = new ethers.Contract(AAVE_LENDING_POOL_ADDRESS.value, lendingPoolAbi, signer.value)
+      const contract = new ethers.Contract(
+        AAVE_LENDING_POOL_ADDRESS.value,
+        lendingPoolAbi,
+        signer.value
+      ) as unknown as AaveLendingPoolContract
       const amount = ethers.utils.parseUnits(`${amountInTokens.value}`, pool.value.decimals)
-
-      const depositCall = await lendingPoolContract.populateTransaction.deposit(
-        pool.value.underlyingAsset,
-        amount,
-        account.value,
-        0
-      )
-      depositCall.gasLimit = await provider.value.estimateGas(depositCall)
-      depositCall.gasPrice = await provider.value.getGasPrice()
-      const depositResult = await signer.value.sendTransaction(depositCall)
-      const resp = await depositResult.wait()
-      return { isCompleted: true, receipt: resp }
-    } catch (error) {
-      return { isCompleted: false, receipt: error }
-    }
-  }
-
-  async function borrowWeb3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
-    try {
-      const lendingPoolContract = new ethers.Contract(AAVE_LENDING_POOL_ADDRESS.value, lendingPoolAbi, signer.value)
-      const amount = ethers.utils.parseUnits(`${amountInTokens.value}`, pool.value.decimals)
-      const borrowCall = await lendingPoolContract.populateTransaction.borrow(
-        pool.value.underlyingAsset,
-        amount,
-        2, // the type of borrow debt. Stable: 1, Variable: 2
-        0, // referral code
-        account.value
-      )
-      const txGasLimit = await provider.value.estimateGas(borrowCall)
-      const txGasPrice = await provider.value.getGasPrice()
-      borrowCall.gasLimit = txGasLimit
-      borrowCall.gasPrice = txGasPrice
-      const txResult = await signer.value.sendTransaction(borrowCall)
-      const resp = await txResult.wait()
+      const borrowCall = await contract.borrow(pool.value.underlyingAsset, amount, 2, 0, account.value)
+      const resp = await borrowCall.wait()
       return { isCompleted: true, receipt: resp }
     } catch (err) {
       return { isCompleted: false, receipt: err }
     }
   }
 
-  async function withdrawWeb3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
+  async function withdrawERC20Web3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
     try {
-      const lendingPoolContract = new ethers.Contract(AAVE_LENDING_POOL_ADDRESS.value, lendingPoolAbi, signer.value)
+      const contract = new ethers.Contract(
+        AAVE_LENDING_POOL_ADDRESS.value,
+        lendingPoolAbi,
+        signer.value
+      ) as unknown as AaveLendingPoolContract
       const amount = ethers.utils.parseUnits(`${amountInTokens.value}`, pool.value.decimals)
-      const withdrawCall = await lendingPoolContract.populateTransaction.withdraw(
-        pool.value.underlyingAsset,
-        amount,
-        account.value
-      )
-      const txGasLimit = await provider.value.estimateGas(withdrawCall)
-      const txGasPrice = await provider.value.getGasPrice()
-      withdrawCall.gasLimit = txGasLimit
-      withdrawCall.gasPrice = txGasPrice
-      const txResult = await signer.value.sendTransaction(withdrawCall)
-      const resp = await txResult.wait()
+      const withdrawCall = await contract.withdraw(pool.value.underlyingAsset, amount, account.value)
+      const resp = await withdrawCall.wait()
       return { isCompleted: true, receipt: resp }
     } catch (err) {
       return { isCompleted: false, receipt: err }
     }
   }
 
-  async function repayWeb3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
+  async function repayERC20Web3Call(): Promise<{ isCompleted: boolean; receipt: any }> {
     try {
-      const lendingPoolContract = new ethers.Contract(AAVE_LENDING_POOL_ADDRESS.value, lendingPoolAbi, signer.value)
+      const contract = new ethers.Contract(
+        AAVE_LENDING_POOL_ADDRESS.value,
+        lendingPoolAbi,
+        signer.value
+      ) as unknown as AaveLendingPoolContract
       const amount = ethers.utils.parseUnits(`${amountInTokens.value}`, pool.value.decimals)
-      const repayCall = await lendingPoolContract.populateTransaction.repay(
-        pool.value.underlyingAsset,
-        amount,
-        2,
-        account.value
-      )
-      const txGasLimit = await provider.value.estimateGas(repayCall)
-      const txGasPrice = await provider.value.getGasPrice()
-      repayCall.gasLimit = txGasLimit
-      repayCall.gasPrice = txGasPrice
-      const txResult = await signer.value.sendTransaction(repayCall)
-      const resp = await txResult.wait()
+      const repayCall = await contract.repay(pool.value.underlyingAsset, amount, 2, account.value)
+      const resp = await repayCall.wait()
       return { isCompleted: true, receipt: resp }
     } catch (err) {
       return { isCompleted: false, receipt: err }
@@ -220,20 +209,15 @@ export default function useAaveTransactions(pool: Ref<AavePoolModel>, amountInTo
 
   async function deposit() {
     txData.loading = true
-    const isApproved = await isSpendingApproved()
-    if (!isApproved) {
-      const { isCompleted, receipt } = await approveMaxSpendingWeb3Call()
-      if (!isCompleted) {
-        txData.isCompleted = isCompleted
-        txData.receipt = receipt
-        return
+    const isNative = isNativeToken(chainId.value ?? 1, pool.value.symbol)
+    if (!isNative) {
+      const allowed = await allowedSpending(pool.value.underlyingAsset, AAVE_LENDING_POOL_ADDRESS.value)
+      if (allowed < amountInTokens.value) {
+        await approveMaxSpending(pool.value.underlyingAsset, AAVE_LENDING_POOL_ADDRESS.value)
       }
     }
-
     let result: { isCompleted: boolean; receipt: any }
-    ;['ETH', 'MATIC'].includes(pool.value.symbol)
-      ? (result = await depositETHWeb3Call())
-      : (result = await depositWeb3Call())
+    isNative ? (result = await depositETHWeb3Call()) : (result = await depositERC20Web3Call())
     txData.isCompleted = result.isCompleted
     txData.receipt = result.receipt
     txData.loading = false
@@ -241,7 +225,7 @@ export default function useAaveTransactions(pool: Ref<AavePoolModel>, amountInTo
 
   async function borrow() {
     txData.loading = true
-    const { isCompleted, receipt } = await borrowWeb3Call()
+    const { isCompleted, receipt } = await borrowERC20Web3Call()
     txData.isCompleted = isCompleted
     txData.receipt = receipt
     txData.loading = false
@@ -249,26 +233,43 @@ export default function useAaveTransactions(pool: Ref<AavePoolModel>, amountInTo
 
   async function repay() {
     txData.loading = true
-    const isApproved = await isSpendingApproved()
-    if (!isApproved) {
-      const { isCompleted, receipt } = await approveMaxSpendingWeb3Call()
-      if (!isCompleted) {
-        txData.isCompleted = isCompleted
-        txData.receipt = receipt
-        return
+    const isNative = isNativeToken(chainId.value ?? 1, pool.value.symbol)
+    if (!isNative) {
+      const allowed = await allowedSpending(pool.value.underlyingAsset, AAVE_LENDING_POOL_ADDRESS.value)
+      if (allowed < amountInTokens.value) {
+        await approveMaxSpending(pool.value.underlyingAsset, AAVE_LENDING_POOL_ADDRESS.value)
+      }
+    } else {
+      const allowed = await allowedSpending(AWETH_ADDRESS.value, AAVE_WETH_GATEWAY_ADDRESS.value)
+      if (allowed < amountInTokens.value) {
+        await approveMaxSpending(AWETH_ADDRESS.value, AAVE_WETH_GATEWAY_ADDRESS.value)
       }
     }
-    const { isCompleted, receipt } = await repayWeb3Call()
-    txData.isCompleted = isCompleted
-    txData.receipt = receipt
+    let result: { isCompleted: boolean; receipt: any }
+    isNative ? (result = await repayETHWeb3Call()) : (result = await repayERC20Web3Call())
+    txData.isCompleted = result.isCompleted
+    txData.receipt = result.receipt
     txData.loading = false
   }
 
   async function withdraw() {
     txData.loading = true
-    const { isCompleted, receipt } = await withdrawWeb3Call()
-    txData.isCompleted = isCompleted
-    txData.receipt = receipt
+    const isNative = isNativeToken(chainId.value ?? 1, pool.value.symbol)
+    if (!isNative) {
+      const allowed = await allowedSpending(pool.value.underlyingAsset, AAVE_LENDING_POOL_ADDRESS.value)
+      if (allowed < amountInTokens.value) {
+        await approveMaxSpending(pool.value.underlyingAsset, AAVE_LENDING_POOL_ADDRESS.value)
+      }
+    } else {
+      const allowed = await allowedSpending(AWETH_ADDRESS.value, AAVE_WETH_GATEWAY_ADDRESS.value)
+      if (allowed < amountInTokens.value) {
+        await approveMaxSpending(AWETH_ADDRESS.value, AAVE_WETH_GATEWAY_ADDRESS.value)
+      }
+    }
+    let result: { isCompleted: boolean; receipt: any }
+    isNative ? (result = await withdrawETHWeb3Call()) : (result = await withdrawERC20Web3Call())
+    txData.isCompleted = result.isCompleted
+    txData.receipt = result.receipt
     txData.loading = false
   }
   return { txLoading, receipt, isTxMined, deposit, borrow, repay, withdraw, resetToDefault }
